d7f606cd53adf15eabe9966febaca568a052a33e
Parth Rupareliya <parth30.hitinfotech@gmail.com>
2023-04-06 17:07:33 +0530

    android fix

--- android/src/main/kotlin/com/hit/screen/corners/screen_corner/ScreenCornerPlugin.kt
+++ android/src/main/kotlin/com/hit/screen/corners/screen_corner/ScreenCornerPlugin.kt
@@ -3,6 +3,7 @@ package com.hit.screen.corners.screen_corner
 import android.os.Build
 import android.view.RoundedCorner
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
 
 import io.flutter.embedding.engine.plugins.FlutterPlugin
 import io.flutter.plugin.common.MethodCall
@@ -11,36 +12,66 @@ import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
 
 /** ScreenCornerPlugin */
-class ScreenCornerPlugin : FlutterPlugin, MethodCallHandler, FlutterActivity() {
-    private lateinit var channel: MethodChannel
+class ScreenCornerPlugin : FlutterActivity() {
+//    private lateinit var channel: MethodChannel
+    private val CHANNEL = "hit:screen_corner";
 
-    override fun onAttachedToEngine(flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
-        channel = MethodChannel(flutterPluginBinding.binaryMessenger, "hit:screen_corner")
-        channel.setMethodCallHandler(this)
-    }
+//    override fun onAttachedToEngine(flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
+//        channel = MethodChannel(flutterPluginBinding.binaryMessenger)
+//        channel.setMethodCallHandler(this)
+//    }
 
-    override fun onMethodCall(call: MethodCall, result: Result) {
-        if (call.method == "getCorners") {
-            val borderRadius = getScreenCorners()
-            if (borderRadius != null) {
-                result.success(borderRadius)
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+        MethodChannel(
+            flutterEngine.dartExecutor.binaryMessenger,
+            CHANNEL
+        ).setMethodCallHandler { call, result ->
+            if (call.method == "getCorners") {
+                val borderRadius = getScreenCorners()
+                if (borderRadius != null) {
+                    result.success(borderRadius)
+                } else {
+                    result.error(
+                        "UNAVAILABLE",
+                        "Border radius fetching is only supported from API 33 only!",
+                        null
+                    )
+                }
             } else {
-                result.error("UNAVAILABLE", "Border radius fetching is only supported from API 33 only!", null)
+                result.notImplemented()
             }
-        } else {
-            result.notImplemented()
         }
     }
 
+//    override fun onMethodCall(call: MethodCall, result: Result) {
+//        if (call.method == "getCorners") {
+//            val borderRadius = getScreenCorners()
+//            if (borderRadius != null) {
+//                result.success(borderRadius)
+//            } else {
+//                result.error(
+//                    "UNAVAILABLE",
+//                    "Border radius fetching is only supported from API 33 only!",
+//                    null
+//                )
+//            }
+//        } else {
+//            result.notImplemented()
+//        }
+//    }
+
     private fun getScreenCorners(): Float? {
         return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
-            window.decorView.rootWindowInsets.getRoundedCorner(RoundedCorner.POSITION_TOP_RIGHT)?.radius?.div(resources.displayMetrics.density)
+            window.decorView.rootWindowInsets.getRoundedCorner(RoundedCorner.POSITION_TOP_RIGHT)?.radius?.div(
+                resources.displayMetrics.density
+            )
         } else {
             return null
         }
     }
 
-    override fun onDetachedFromEngine(binding: FlutterPlugin.FlutterPluginBinding) {
-        channel.setMethodCallHandler(null)
-    }
+//    override fun onDetachedFromEngine(binding: FlutterPlugin.FlutterPluginBinding) {
+//        channel.setMethodCallHandler(null)
+//    }
 }
