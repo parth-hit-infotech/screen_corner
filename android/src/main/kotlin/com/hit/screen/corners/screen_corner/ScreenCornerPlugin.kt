dc0c1f237eebc70b77cde9aa8c2cf6ce71bb240d
Parth Rupareliya <parth30.hitinfotech@gmail.com>
2023-04-06 16:31:22 +0530

    android

--- android/src/main/kotlin/com/hit/screen/corners/screen_corner/ScreenCornerPlugin.kt
+++ android/src/main/kotlin/com/hit/screen/corners/screen_corner/ScreenCornerPlugin.kt
@@ -1,6 +1,8 @@
 package com.hit.screen.corners.screen_corner
 
-import androidx.annotation.NonNull
+import android.os.Build
+import android.view.RoundedCorner
+import io.flutter.embedding.android.FlutterActivity
 
 import io.flutter.embedding.engine.plugins.FlutterPlugin
 import io.flutter.plugin.common.MethodCall
@@ -9,27 +11,36 @@ import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
 
 /** ScreenCornerPlugin */
-class ScreenCornerPlugin: FlutterPlugin, MethodCallHandler {
-  /// The MethodChannel that will the communication between Flutter and native Android
-  ///
-  /// This local reference serves to register the plugin with the Flutter Engine and unregister it
-  /// when the Flutter Engine is detached from the Activity
-  private lateinit var channel : MethodChannel
+class ScreenCornerPlugin : FlutterPlugin, MethodCallHandler, FlutterActivity() {
+    private lateinit var channel: MethodChannel
 
-  override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
-    channel = MethodChannel(flutterPluginBinding.binaryMessenger, "screen_corner")
-    channel.setMethodCallHandler(this)
-  }
-
-  override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) {
-    if (call.method == "getPlatformVersion") {
-      result.success("Android ${android.os.Build.VERSION.RELEASE}")
-    } else {
-      result.notImplemented()
+    override fun onAttachedToEngine(flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
+        channel = MethodChannel(flutterPluginBinding.binaryMessenger, "hit:screen_corner")
+        channel.setMethodCallHandler(this)
     }
-  }
 
-  override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
-    channel.setMethodCallHandler(null)
-  }
+    override fun onMethodCall(call: MethodCall, result: Result) {
+        if (call.method == "getScreenCorners") {
+            val borderRadius = getScreenCorners()
+            if (borderRadius != null) {
+                result.success(borderRadius)
+            } else {
+                result.error("UNAVAILABLE", "Border radius fetching is only supported from API 33 only!", null)
+            }
+        } else {
+            result.notImplemented()
+        }
+    }
+
+    private fun getScreenCorners(): Float? {
+        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            window.decorView.rootWindowInsets.getRoundedCorner(RoundedCorner.POSITION_TOP_RIGHT)?.radius?.div(resources.displayMetrics.density)
+        } else {
+            return null
+        }
+    }
+
+    override fun onDetachedFromEngine(binding: FlutterPlugin.FlutterPluginBinding) {
+        channel.setMethodCallHandler(null)
+    }
 }
